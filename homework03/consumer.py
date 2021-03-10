1df553ab7ad3dc2ad6fc98c01301387c3d650ef5
sierraog <sierrao@utexas.edu>
2021-03-10 17:17:18 -0600

    updated consumer file with Kyle's endpoints

--- homework03/consumer.py
+++ homework03/consumer.py
@@ -1,12 +1,16 @@
 import requests
 
-response = requests.get(url="http://localhost:5050/animals")
-print(response.status_code)
-print(response.json())
-print(response.headers)
-
 response = requests.get(url="http://localhost:5022/animals")
 print(response.status_code)
 print(response.json())
 print(response.headers)
 
+response = requests.get(url="http://localhost:5022/animals/body/dodo")
+print(response.status_code)
+print(response.json())
+print(response.headers)
+
+response = requests.get(url="http://localhost:5022/animals/tails/12")
+print(response.status_code)
+print(response.json())
+print(response.headers)
